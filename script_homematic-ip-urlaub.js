1c5d5f8730daa3b110e3cbe7c5476fe4693dcf05
Mic-M <39859059+Mic-M@users.noreply.github.com>
2020-01-25 16:50:46 +0100

    Version 0.2

--- script_homematic-ip-urlaub.js
+++ script_homematic-ip-urlaub.js
@@ -15,18 +15,26 @@
  * Support: https://forum.iobroker.net/topic/22140/
  * ----------------------------------------------------
  * Change Log:
+ *  0.2 Mic + Support creating states under 0_userdata.0
  *  0.1 Mic - Initial Release
  * ----------------------------------------------------
  * Autor: Mic (ioBroker-Forum) / Mic-M (Github)
  ******************************************************************************/
-
+/*
+ * VORAUSSETZUNG:
+ * In der Instanz des JavaScript-Adapters die Option [Erlaube das Kommando "setObject"] aktivieren.
+ * Das ist notwendig, damit die Datenpunkte unterhalb von 0_userdata.0 angelegt werden können.
+ * https://github.com/Mic-M/iobroker.createUserStates
+ * Wer das nicht möchte: bitte Script-Version 0.1 verwenden.
+ */
 
 /*******************************************************************************
  * Konfiguration: Pfade / Datenpunkte
  ******************************************************************************/
 
-// Pfad, unter dem die Datenpunkte angelegt werden sollen, mit abschließendem Punkt "."
-const STATEPATH = 'javascript.0.Heizung._Urlaubsmodus.';
+// Pfad, unter dem die States (Datenpunkte) in den Objekten angelegt werden.
+// Es wird die Anlage sowohl unterhalb '0_userdata.0' als auch 'javascript.x' unterstützt.
+const STATEPATH = '0_userdata.0.Heizung.Urlaubsmodus';
 
 // Wie viele unterschiedliche Urlaube zur Vorausplanung?
 // Mehr als 10-20 machen wohl keinen Sinn, es sei denn ständig unterwegs :-)
@@ -60,21 +68,35 @@ const SCHEDULE_PLAN = '2 0 * * *'; // Jeden Tag um 0:02.
  ******************************************************************************/
 
 /*******************************************************************************
+ * Global variables and constants
+******************************************************************************/
+
+// Final state path
+const FINAL_STATE_LOCATION = validateStatePath(STATEPATH, false);
+const FINAL_STATE_PATH = validateStatePath(STATEPATH, true) + '.'; // we add a trailing dot
+
+
+
+/*******************************************************************************
  * Initiale Function
  *******************************************************************************/
 init();
 function init() {
     
-    // Create states
-    createScriptStates();
+    // Create all script states
+    createUserStates(FINAL_STATE_LOCATION, false, buildScriptStates(), function() {
+        // -- All states created, so we continue by using callback
 
-    // Update Status 'isActive'
-    setTimeout(updateAll, 2000);
+        // Update Status 'isActive'
+        setTimeout(updateAll, 2000);
 
-    // Call main function later
-    setTimeout(main, 5000);
+        // Call main function later
+        setTimeout(main, 5000);
+    
+    });
 
 }
+
 let mSchedule;
 function main() {
 
@@ -87,7 +109,7 @@ function main() {
     /**
      * 2. Date-Datenpunkte überwachen und bei Änderung ebenfalls Schedule durchführen / aktualisieren
      */
-    let regExp = new RegExp('^' + escapeRegExp(STATEPATH) + '.*Date.*');
+    let regExp = new RegExp('^' + escapeRegExp(FINAL_STATE_PATH) + '.*Date.*');
     on({id: regExp, change:'ne'}, function (obj) {
         updateAll();
     });
@@ -95,7 +117,7 @@ function main() {
     /**
      * 3. Datenpunkt 'IsHoliday' überwachen und bei Änderung schalten wir Urlaubsmodus an/aus von allen Thermostaten.
      */
-    on({id: STATEPATH + 'IsHoliday', change:'ne'}, function (obj) {
+    on({id: FINAL_STATE_PATH + 'IsHoliday', change:'ne'}, function (obj) {
         let hmProfile;
         let mytext;
         if (obj.state.val) {
@@ -134,7 +156,7 @@ function updateAll() {
         let holidayStatus = updateStatus(i);
         if (holidayStatus) isHoliday = true;
     }
-    setState(STATEPATH + 'IsHoliday', isHoliday);
+    setState(FINAL_STATE_PATH + 'IsHoliday', isHoliday);
     log('Heizungs-Script Urlaubsmodus: alle Zeiträume geprüft und "isActive" von allen aktualisiert.')
 }
 
@@ -145,26 +167,28 @@ function updateAll() {
  */
 function updateStatus(scheduleNumber) {
     let isHoliday = false;
-    if ( G_isDateInRange('now', getState(STATEPATH + scheduleNumber + '-Date-Start').val, getState(STATEPATH + scheduleNumber + '-Date-End').val) ) {
-        setState(STATEPATH + scheduleNumber + '-IsActive', true);
+    if ( isDateInRange('now', getState(FINAL_STATE_PATH + scheduleNumber + '-Date-Start').val, getState(FINAL_STATE_PATH + scheduleNumber + '-Date-End').val) ) {
+        setState(FINAL_STATE_PATH + scheduleNumber + '-IsActive', true);
         isHoliday = true;
     } else {
-        setState(STATEPATH + scheduleNumber + '-IsActive', false);
+        setState(FINAL_STATE_PATH + scheduleNumber + '-IsActive', false);
     }      
     return isHoliday;
 }
 
 
 /**
- * Create states we need.
+ * Build script states.
  */
-function createScriptStates() {
+function buildScriptStates() {
+    let finalStates = [];
     for (let i = 1; i <= HOLIDAY_QTY; i++) {
-        createState(STATEPATH + i + '-Date-Start', {'name':'Urlaubsmodus ' + i + ' - Anfang', 'type':'string', 'read':true, 'write':true, 'role':'state', 'def':'2019-01-23'});
-        createState(STATEPATH + i + '-Date-End',   {'name':'Urlaubsmodus ' + i + ' - Ende', 'type':'string', 'read':true, 'write':true, 'role':'state', 'def':'2019-01-29'});
-        createState(STATEPATH + i + '-IsActive',   {'name':'Urlaubsmodus ' + i + ' - Aktiv?', 'type':'boolean', 'read':true, 'write':false, 'role':'state', 'def':false});
+        finalStates.push([FINAL_STATE_PATH + i + '-Date-Start', {'name':'Urlaubsmodus ' + i + ' - Anfang', 'type':'string', 'read':true, 'write':true, 'role':'state', 'def':'2019-01-23'}]);
+        finalStates.push([FINAL_STATE_PATH + i + '-Date-End',   {'name':'Urlaubsmodus ' + i + ' - Ende', 'type':'string', 'read':true, 'write':true, 'role':'state', 'def':'2019-01-29'}]);
+        finalStates.push([FINAL_STATE_PATH + i + '-IsActive',   {'name':'Urlaubsmodus ' + i + ' - Aktiv?', 'type':'boolean', 'read':true, 'write':false, 'role':'state', 'def':false}]);
     }
-    createState(STATEPATH + 'IsHoliday',   {'name':'Ist aktuell Urlaub?', 'type':'boolean', 'read':true, 'write':false, 'role':'state', 'def':false});
+    finalStates.push([FINAL_STATE_PATH + 'IsHoliday',   {'name':'Ist aktuell Urlaub?', 'type':'boolean', 'read':true, 'write':false, 'role':'state', 'def':false}]);
+    return finalStates;
 }
 
 
@@ -174,4 +198,182 @@ function createScriptStates() {
  */
 function escapeRegExp(string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+
+/**
+ * Abfrage, ob ein Datum innerhalb eines Bereichs ist.
+ * Angeregt von: https://forum.iobroker.net/topic/2289/
+ * Zum Aufruf ist das ISO-Datumsformat JJJJ-MM-TT erforderlich, z.B. 2019-12-24.
+ * Autor: Mic
+ * Version: 0.2 - 28-Apr-2019
+ * Gepostet unter: https://forum.iobroker.net/post/256400
+ * @param {string}   strDateToCheck       Das zu prüfende Datum; 
+ *                                        Falls aktuelles Datum geprüft werden soll: 'now' eingeben.
+ * @param {string}   strDateFirst         Datum: Zeitraum erster Tag.
+ * @param {string}   strDateLast          Datum: Zeitraum letzter Tag.
+ */
+function isDateInRange(strDateToCheck, strDateFirst, strDateLast) {
+    // Prepare the date we want to check. Either current date, or provided date in variable strDateToCheck
+    let dtToCheck;
+    if (strDateToCheck == 'now') {
+        dtToCheck = new Date();
+    } else {
+         dtToCheck = new Date(strDateToCheck);
+    }
+    dtToCheck.setHours(0, 0, 0, 0); // Same basis for hours, minutes etc. as for all dates
+    let dtFirst = new Date(strDateFirst);
+    dtFirst.setHours(0, 0, 0, 0);
+    let dtLast = new Date(strDateLast);
+    dtLast.setHours(0, 0, 0, 0);
+    let isInRange = false;
+    /* Don't need this, has weird effects if just one date is being changed */
+    /*
+    if (dtLast < dtFirst) {
+        // We need to swap the variables since user mixed up first and last date.
+        let dtTempFirst = dtFirst;
+        dtFirst = dtLast;
+        dtLast = dtTempFirst;
+        log('Function isDateInRange: first date is later than last date, but the function corrected this accordingly.', 'debug');
+    }
+    */
+    if ( (dtLast >= dtFirst) && (dtToCheck >= dtFirst) && (dtToCheck <= dtLast) )  {
+        isInRange = true;
+    }
+    return isInRange;
+}
+
+
+/**
+ * For a given state path, we extract the location '0_userdata.0' or 'javascript.0' or add '0_userdata.0', if missing.
+ * @param {string}  path            Like: 'Computer.Control-PC', 'javascript.0.Computer.Control-PC', '0_userdata.0.Computer.Control-PC'
+ * @param {boolean} returnFullPath  If true: full path like '0_userdata.0.Computer.Control-PC', if false: just location like '0_userdata.0' or 'javascript.0'
+ * @return {string}                 Path
+ */
+function validateStatePath(path, returnFullPath) {
+    if (path.startsWith('.')) path = path.substr(1);    // Remove first dot
+    if (path.endsWith('.'))   path = path.slice(0, -1); // Remove trailing dot
+    if (path.length < 1) log('Provided state path is not valid / too short.', 'error')
+    let match = path.match(/^((javascript\.([1-9][0-9]|[0-9])\.)|0_userdata\.0\.)/);
+    let location = (match == null) ? '0_userdata.0' : match[0].slice(0, -1); // default is '0_userdata.0'.
+    if(returnFullPath) {
+        return (path.indexOf(location) == 0) ? path : (location + '.' + path);
+    } else {
+        return location;
+    }
+}
+
+
+/**
+ * Create states under 0_userdata.0 or javascript.x
+ * Current Version:     https://github.com/Mic-M/iobroker.createUserStates
+ * Support:             https://forum.iobroker.net/topic/26839/
+ * Autor:               Mic (ioBroker) | Mic-M (github)
+ * Version:             1.0 (17 January 2020)
+ * Example:
+ * -----------------------------------------------
+    let statesToCreate = [
+        ['Test.Test1', {'name':'Test 1', 'type':'string', 'read':true, 'write':true, 'role':'info', 'def':'Hello' }],
+        ['Test.Test2', {'name':'Test 2', 'type':'string', 'read':true, 'write':true, 'role':'info', 'def':'Hello' }],
+    ];
+    createUserStates('0_userdata.0', false, statesToCreate);
+ * -----------------------------------------------
+ * PLEASE NOTE: Per https://github.com/ioBroker/ioBroker.javascript/issues/474, the used function setObject() 
+ *              executes the callback PRIOR to completing the state creation. Therefore, we use a setTimeout and counter. 
+ * -----------------------------------------------
+ * @param {string} where          Where to create the state: e.g. '0_userdata.0' or 'javascript.x'.
+ * @param {boolean} force         Force state creation (overwrite), if state is existing.
+ * @param {array} statesToCreate  State(s) to create. single array or array of arrays
+ * @param {object} [callback]     Optional: a callback function -- This provided function will be executed after all states are created.
+ */
+function createUserStates(where, force, statesToCreate, callback = undefined) {
+ 
+    const WARN = false; // Throws warning in log, if state is already existing and force=false. Default is false, so no warning in log, if state exists.
+    const LOG_DEBUG = false; // To debug this function, set to true
+    // Per issue #474 (https://github.com/ioBroker/ioBroker.javascript/issues/474), the used function setObject() executes the callback 
+    // before the state is actual created. Therefore, we use a setTimeout and counter as a workaround.
+    // Increase this to 100, if it is not working.
+    const DELAY = 50; // Delay in milliseconds (ms)
+
+
+    // Validate "where"
+    if (where.endsWith('.')) where = where.slice(0, -1); // Remove trailing dot
+    if ( (where.match(/^javascript.([0-9]|[1-9][0-9])$/) == null) && (where.match(/^0_userdata.0$/) == null) ) {
+        log('This script does not support to create states under [' + where + ']', 'error');
+        return;
+    }
+
+    // Prepare "statesToCreate" since we also allow a single state to create
+    if(!Array.isArray(statesToCreate[0])) statesToCreate = [statesToCreate]; // wrap into array, if just one array and not inside an array
+
+    let numStates = statesToCreate.length;
+    let counter = -1;
+    statesToCreate.forEach(function(param) {
+        counter += 1;
+        if (LOG_DEBUG) log ('[Debug] Currently processing following state: [' + param[0] + ']');
+
+        // Clean
+        let stateId = param[0];
+        if (! stateId.startsWith(where)) stateId = where + '.' + stateId; // add where to beginning of string
+        stateId = stateId.replace(/\.*\./g, '.'); // replace all multiple dots like '..', '...' with a single '.'
+        const FULL_STATE_ID = stateId;
+
+        if( ($(FULL_STATE_ID).length > 0) && (existsState(FULL_STATE_ID)) ) { // Workaround due to https://github.com/ioBroker/ioBroker.javascript/issues/478
+            // State is existing.
+            if (WARN && !force) log('State [' + FULL_STATE_ID + '] is already existing and will no longer be created.', 'warn');
+            if (!WARN && LOG_DEBUG) log('[Debug] State [' + FULL_STATE_ID + '] is already existing. Option force (=overwrite) is set to [' + force + '].');
+
+            if(!force) {
+                // State exists and shall not be overwritten since force=false
+                // So, we do not proceed.
+                numStates--;
+                if (numStates === 0) {
+                    if (LOG_DEBUG) log('[Debug] All states successfully processed!');
+                    if (typeof callback === 'function') { // execute if a function was provided to parameter callback
+                        if (LOG_DEBUG) log('[Debug] An optional callback function was provided, which we are going to execute now.');
+                        return callback();
+                    }
+                } else {
+                    // We need to go out and continue with next element in loop.
+                    return; // https://stackoverflow.com/questions/18452920/continue-in-cursor-foreach
+                }
+            } // if(!force)
+        }
+
+        /************
+         * State is not existing or force = true, so we are continuing to create the state through setObject().
+         ************/
+        let obj = {};
+        obj.type = 'state';
+        obj.native = {};
+        obj.common = param[1];
+        setObject(FULL_STATE_ID, obj, function (err) {
+            if (err) {
+                log('Cannot write object for state [' + FULL_STATE_ID + ']: ' + err);
+            } else {
+                if (LOG_DEBUG) log('[Debug] Now we are creating new state [' + FULL_STATE_ID + ']')
+                let init = null;
+                if(param[1].def === undefined) {
+                    if(param[1].type === 'number') init = 0;
+                    if(param[1].type === 'boolean') init = false;
+                    if(param[1].type === 'string') init = '';
+                } else {
+                    init = param[1].def;
+                }
+                setTimeout(function() {
+                    setState(FULL_STATE_ID, init, true, function() {
+                        if (LOG_DEBUG) log('[Debug] setState durchgeführt: ' + FULL_STATE_ID);
+                        numStates--;
+                        if (numStates === 0) {
+                            if (LOG_DEBUG) log('[Debug] All states processed.');
+                            if (typeof callback === 'function') { // execute if a function was provided to parameter callback
+                                if (LOG_DEBUG) log('[Debug] Function to callback parameter was provided');
+                                return callback();
+                            }
+                        }
+                    });
+                }, DELAY + (20 * counter) );
+            }
+        });
+    });
 }
